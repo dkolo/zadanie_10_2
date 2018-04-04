87dd1d88d022f7db4a11a793910c05258ef42c6f
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-04-04 14:48:26 +0200

    Fix bug with carousel animation

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ $(function() {
     function changeSlide() {
         carouselList.animate({ 'marginLeft': -400 }, 500, moveFirstSlide);
     }
+    function previousSlide() {
+        carouselList.animate({ 'marginLeft': 400 }, 500, moveFirstSlide);
+    } 
     function moveFirstSlide() {
     	var firstItem = carouselList.find('li:first');
 			lastItem = carouselList.find('li:last');
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
     }
+
+    var moveLeft = $('.arrow-right'),
+    	moveRight = $('.arrow-left');
+    moveLeft.click(changeSlide);
+    moveRight.click(previousSlide, setInterval(changeSlide, 5000));
 });
