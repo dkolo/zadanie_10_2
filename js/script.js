07d01c4170c5b0fbfb2d7db2da57b7a91cd1a8de
Dominik Kolodziej <kolodziej.dominik@gmail.com>
2018-06-08 14:03:31 +0200

    Add event handlers to arrows and add clearInterval function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,33 @@
-$(function() {
-    var carouselList = $('#carousel ul');
-    setInterval(changeSlide, 3000);
+$(function slider() {
+    var carouselList = $('#carousel ul'),
+        interval = setInterval(changeSlide, 3000);
 
     function changeSlide() {
         carouselList.animate({ 'marginLeft': -400 }, 500, moveFirstSlide);
     }
+
     function previousSlide() {
-        carouselList.animate({ 'marginLeft': 400 }, 500, moveFirstSlide);
-    } 
-    function moveFirstSlide() {
-    	var firstItem = carouselList.find('li:first');
-			lastItem = carouselList.find('li:last');
-		lastItem.after(firstItem);
-		carouselList.css({marginLeft:0});
+        var firstItem = carouselList.find('li:first'),
+            lastItem = carouselList.find('li:last');
+        firstItem.before(lastItem);
+        carouselList.css({ marginLeft: -400 });
+        carouselList.animate({ 'marginLeft': 0 });
     }
 
-    var moveLeft = $('.arrow-right'),
-    	moveRight = $('.arrow-left');
-    moveLeft.click(changeSlide);
-    moveRight.click(previousSlide);
+    function moveFirstSlide() {
+        var firstItem = carouselList.find('li:first'),
+            lastItem = carouselList.find('li:last');
+        lastItem.after(firstItem);
+        carouselList.css({ marginLeft: 0 });
+
+    }
+
+    $('.arrow-right').click(function (){
+        changeSlide();
+        clearInterval(interval);
+    });
+    $('.arrow-left').click(function (){
+        previousSlide();
+        clearInterval(interval);
+    });
 });
